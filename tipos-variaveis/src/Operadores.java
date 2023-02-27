cc400fac235a84d391c53ec0751244d4c839d30b
laissalviatoclaudiano <laissalviatoclaudiano@gmail.com>
2023-02-27 14:01:09 -0300

    Adicionando conteúdo ao projeto

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,45 @@
+public class Operadores {
+    
+    public static void main(String[] args ) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+        
+        concatenacao = "1"+(1+1+1);
+        
+        System.out.println(concatenacao);
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente de número dois? " + simNao);
+
+        boolean condicao1=true;
+        boolean condicao2=false;
+
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira");
+        }
+    }
+}
